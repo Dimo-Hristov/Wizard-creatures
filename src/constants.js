144662cfb1fd095bd01ac0c4177c3a4da2b879b9
dimo_xrisrtov@protonmail.com <dimo_xristov@protonmail.com>
2024-03-10 13:59:43 +0200

    connect mongodb cloud

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     PORT: 3000,
-    DB_URL: 'mongodb://127.0.0.1:27017/wizard-creatures',
+    DB_URL: 'mongodb+srv://dimoxristov:SxPO93uNKuIKG1kY@wizard-creatures.rl9o3uj.mongodb.net/?retryWrites=true&w=majority&appName=wizard-creatures',
     SECRET: '2a083d4b-9fa1-417c-afa6-1bd9b0a6084a'
 };
 
